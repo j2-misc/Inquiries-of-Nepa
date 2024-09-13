e657d6bbba5b4d94917fe44c02bad6b8f88581cc
j2-misc <data.pool.2045@gmail.com>
2024-09-12 23:50:54 -0700

    Quartz sync: Sep 12, 2024, 11:50 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Inquiries of Nepa",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "j2-misc.github.io/Inquiries-of-Nepa",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
